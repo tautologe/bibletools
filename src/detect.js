250f2438cc28c5843096e1aa53e55baf1d8a9d0f
tautologe <tm-login+git@mailbox.org>
2017-02-26 00:22:50 +0100

    added range detection, eslint config

--- src/detect.js
+++ src/detect.js
@@ -1,13 +1,13 @@
 export default (text) => {
     const books = [
-        ['Gen','1. Mo','1 Mo'],
-        ['Ex'],
-        ['Lev'],
-        ['Num'],
-        ['Dtn'],
-        ['Jos'],
-        ['Ri'],
-        ['Rut'],
+        ['Gen','Genesis','1. Mo','1 Mo','1. Mose', '1 Mose'],
+        ['Ex','Exodus','2. Mo','2 Mo','2. Mose', '2 Mose'],
+        ['Lev','Levitikus','3. Mo','3 Mo','3. Mose', '3 Mose'],
+        ['Num','Numeri','4. Mo','4 Mo','4. Mose', '4 Mose'],
+        ['Dtn','Deuteronomium','5. Mo','5 Mo','5. Mose', '5 Mose'],
+        ['Jos', 'Josua'],
+        ['Ri', 'Richter'],
+        ['Rut', 'Ruth'],
         ['1 Sam'],
         ['2 Sam'],
         ['1 Kön'],
@@ -17,15 +17,15 @@ export default (text) => {
         ['Esra'],
         ['Neh'],['Tob'],['Jdt'],
         ['Est'],['1 Makk'], ['2 Makk'],
-        ['Ijob'],
-        ['Ps'],
+        ['Ijob','Hiob'],
+        ['Ps','Psalm','Psalmen','Psalter'],
         ['Spr'],
-        ['Koh'],
+        ['Koh','Kohelet','Prediger','Pred'],
         ['Hld'],['Weish'],['Sir'],
         ['Jes'],
         ['Jer'],
         ['Klgl'],['Bar'],
-        ['Ez'],
+        ['Ez','Ezechiel','Hes','Hesekiel'],
         ['Dan'],
         ['Hos'],
         ['Joel'],
@@ -71,7 +71,12 @@ export default (text) => {
     const flatten = (arrays) => [].concat.apply([], arrays);
     const escapeRegExp = (str) => str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 
+    // regexp components
     const bookRegExpString = `(${flatten(books).map(escapeRegExp).join('|')})`;
-    const re = new RegExp(`${bookRegExpString}\.? [0-9]{1,3},[0-9]{1,3}`, 'g');
+    const chapterWithOptionalVerse = '[0-9]{1,3}(,[0-9]{1,3})?';
+    const optionalRange = '(-[0-9]{1,3}(,[0-9]{1,3})?)?';
+    const optionaFollowing = 'f?f?';
+
+    const re = new RegExp(`${bookRegExpString}\.? ${chapterWithOptionalVerse}${optionalRange}${optionaFollowing}`, 'g');
     return text.match(re) || [];
 }
--- test/detect_spec.js
+++ test/detect_spec.js
@@ -1,9 +1,33 @@
 import detect from '../src/detect';
 import assert from 'assert';
 
-describe('link detection', function () {
-    it('should detect simple pattern', function () {
-        assert.deepEqual(detect('Es steht in Gen 3,12 geschrieben'), ['Gen 3,12']);
+describe('link detector', function () {
+    describe('should detect', function () {
+        it('should detect simple chapters', function () {
+            assert.deepEqual(detect('Es steht in Gen 3 geschrieben'), ['Gen 3']);
+        });
+
+        it('should detect simple chapters with verses', function () {
+            assert.deepEqual(detect('Es steht in Gen 3,12 geschrieben'), ['Gen 3,12']);
+        });
+
+        describe('ranges of', function () {
+            it('verses', function () {
+                assert.deepEqual(detect('Es steht in Gen 3,12-15 geschrieben'), ['Gen 3,12-15']);
+            });
+
+            it('chapters', function () {
+                assert.deepEqual(detect('Es steht in Gen 3-5 geschrieben'), ['Gen 3-5']);
+            });
+
+            it('overlapping chapters', function () {
+                assert.deepEqual(detect('Es steht in Gen 3,12-4,2 geschrieben'), ['Gen 3,12-4,2']);
+            });
+        });
+    });
+    
+    it('should detect following annotation', function () {
+        assert.deepEqual(detect('Es steht in Gen 3,12f geschrieben'), ['Gen 3,12f']);
     });
 
     it('should detect multiple links', function () {
