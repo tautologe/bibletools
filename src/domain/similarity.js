c4d9cd3f3577a6d78d01b5994d951cff312fc496
tautologe <tautologe@users.noreply.github.com>
2018-11-15 23:58:17 +0100

    similarity: consider stopwords

--- src/domain/similarity.js
+++ src/domain/similarity.js
@@ -9,12 +9,22 @@ const dot_product = (vektor_a, vektor_b) => {
     }).reduce((sum, next) => sum + next, 0);
 };
 
+const stopWords = ["G2532","G846","G1519","G1151","G3756","G2192", "G1161",
+"G1722", "G1063", "G3754", "G1909", "G2076", "G3004", "G2036", "G4314",
+"G575", "G3956", "G2257", "G5216", "G2443", "G2228", "G4675", "G3588",
+"G3739", "G5209", "G5213", "G5101", "G1096", "G1537", "G3326",
+"H559", "H6440", "H2930", "H5922", "H6925"];
+
 // create strong vektors with absolute occurrences
 const strong_matrix_sparse = []
 const strongPromise = fetch('repo/bible/strong_stats2.json').then(result => result.json())
 strongPromise.then(json => json.forEach((book, book_index) => {
     strong_matrix_sparse[book_index] = []; 
-    book.strongs.forEach(strong => strong_matrix_sparse[book_index][strong.key.substring(1)] = strong.value);
+    book.strongs.forEach(strong => {
+        if (!stopWords.includes(strong.key)) {
+            strong_matrix_sparse[book_index][strong.key.substring(1)] = strong.value
+        }
+    });
 }));
 
 // normalize strong vektors
